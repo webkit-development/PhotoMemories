360f46704118e4df99ae71966069fda665c67803
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-31 10:06:23 -0400

    Initial Commit

---
+++ PhotoMemories/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PhotoMemories
+//
+//  Created by Kevin Stradtman on 3/31/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
